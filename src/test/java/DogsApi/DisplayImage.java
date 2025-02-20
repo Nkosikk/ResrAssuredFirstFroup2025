4b09975083bd8ece043d0d31ac06bd39ad6fb606
Sisho <sishoharry@gmai.com>
2025-02-20 19:38:38 +0200

    display image

--- src/test/java/DogsApi/DisplayImage.java
+++ src/test/java/DogsApi/DisplayImage.java
@@ -1,18 +1,15 @@
+package DogsApi;
+
 import io.restassured.RestAssured;
 import io.restassured.response.Response;
 
-import static org.hamcrest.Matchers.notNullValue;
-
-public class DogsApI {
+public class DisplayImage {
     public static void main(String[] args) {
-        Response response = RestAssured.get("https://dog.ceo/api/breeds/list/all");
+        Response response = RestAssured.get("https://dog.ceo/api/breeds/image/random");
         System.out.println("Response Status Code: " + response.getStatusCode());
         System.out.println("Response Body: " + response.getBody().asString());
 
         // Verify that the status code is 200
         response.then().assertThat().statusCode(200);
-
-        // Verify that "doberman" is in the list
-        response.then().assertThat().body("message.doberman", notNullValue());
     }
 }
