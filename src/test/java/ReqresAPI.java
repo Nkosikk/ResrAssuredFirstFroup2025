7bc08e2992555c1f75f64da6da40b95fa3f4118c
Itumeleng Morei <itumeleng.morei@hmail.com>
2025-02-20 19:20:41 +0200

    Get requests for Reqres & weather station

---
+++ src/test/java/ReqresAPI.java
@@ -0,0 +1,12 @@
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
+
+public class ReqresAPI {
+    public static void main(String[] args) {
+        Response response = RestAssured.get("https://reqres.in/api/users?page=2");
+        System.out.println("Response Status Code: " + response.getStatusCode());
+        System.out.println("Response Body: " + response.getBody().asString());
+    }
+
+
+}
