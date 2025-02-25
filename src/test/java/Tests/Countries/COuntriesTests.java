ae125e3679f62a498528a5b8df07933dcb39601d
kgaogelo0719 <kgaogelomaluleke@gmail.com>
2025-02-25 23:02:50 +0200

    Created a get request to retrieve information about SA

--- src/test/java/Tests/Countries/COuntriesTests.java
+++ src/test/java/Tests/Countries/COuntriesTests.java
@@ -15,7 +15,7 @@ import static org.hamcrest.Matchers.hasItem;
 public class COuntriesTests {
 
     //ToDO Kelebogile - get countries
-    //ToDO Kgaogelo - get countries - do a test to validate south african sign language is included in the list of official language
+    //ToDO Kgaugelo - get countries - do a test to validate south african sign language is included in the list of official language
     //first get the list of all countries
     @Test
     @Feature("Countries API")
