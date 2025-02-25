06b7937f89c12071052d03ecf722ae02f3af65cd
nkosi <nkosi.cele@simplify.africa>
2025-02-25 19:32:11 +0200

    Created the list of all breeds test

---
+++ src/test/java/Tests/Dogs/DogsTests.java
@@ -0,0 +1,24 @@
+package Tests.Dogs;
+
+
+import io.qameta.allure.*;
+import org.testng.annotations.Test;
+
+import static Common.CommonTestData.Success_Status_Code;
+import static Common.RequestBuilder.getListOfAllBreedsResponse;
+
+@Test
+@Feature("Dogs API")
+@Story("List of dogs")
+public class DogsTests {
+
+    @Description("As an api user i want to see all the dogs list")
+    @Severity(SeverityLevel.BLOCKER)
+    public void getListOfAllBreedsTests(){
+        getListOfAllBreedsResponse().
+                then().
+                assertThat().
+                statusCode(201);
+    }
+
+}
