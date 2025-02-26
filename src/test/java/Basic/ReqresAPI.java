cef3d8b4bc75cc94b429886ca154fde3bb87467c
Itumeleng Morei <itumeleng.morei@hmail.com>
2025-02-26 22:32:38 +0200

    Reqres GET requests

--- src/test/java/Basic/ReqresAPI.java
+++ src/test/java/Basic/ReqresAPI.java
@@ -8,6 +8,14 @@ public class ReqresAPI {
         Response response = RestAssured.get("https://reqres.in/api/users?page=2");
         System.out.println("Response Status Code: " + response.getStatusCode());
         System.out.println("Response Body: " + response.getBody().asString());
+
+        Response response1 = RestAssured.get("https://reqres.in/api/users/2");
+        System.out.println("Response Status Code: " + response1.getStatusCode());
+        System.out.println("Response Body: " + response1.getBody().asString());
+
+        Response response2 = RestAssured.get("https://reqres.in/api/unknown/2");
+        System.out.println("Response Status Code: " + response2.getStatusCode());
+        System.out.println("Response Body: " + response2.getBody().asString());
     }
 
 
