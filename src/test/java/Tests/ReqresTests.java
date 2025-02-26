6d08e421e6815388ea6bc30a9f2bbd6bdae0ecfb
Itumeleng Morei <itumeleng.morei@hmail.com>
2025-02-26 22:14:23 +0200

    allure report changes, description and severity

--- src/test/java/Tests/ReqresTests.java
+++ src/test/java/Tests/ReqresTests.java
@@ -1,5 +1,8 @@
 package Tests;
 
+import io.qameta.allure.Description;
+import io.qameta.allure.Severity;
+import io.qameta.allure.SeverityLevel;
 import org.testng.annotations.Test;
 
 import static Common.CommonTestData.Success_Status_Code;
@@ -7,7 +10,10 @@ import static Common.RequestBuilder.getListOfUsersResponse;
 import static Common.RequestBuilder.getSingleResourceResponse;
 
 @Test
-public class ReqresTests {
+public class ReqresTests{
+@Description("Tests for Reqres API")
+@Severity(SeverityLevel.MINOR)
+
     public void getListOfUsersTests(){
         getListOfUsersResponse().
                 then().
