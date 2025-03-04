1ef589fdf3151ff3113d13b06a934aab1e465fb1
sibonisot <sibonisot@yahoo.com>
2025-03-04 20:04:08 +0200

    created more test data from faker

--- src/test/java/Common/PayloadBuilder.java
+++ src/test/java/Common/PayloadBuilder.java
@@ -2,16 +2,16 @@ package Common;
 
 import org.json.simple.JSONObject;
 
-import static Common.TestDataCreation.first_name;
+import static Common.TestDataCreation.*;
 
 public class PayloadBuilder {
 
     public static JSONObject createEmployeeObject(){
 
         JSONObject empFields = new JSONObject();
-        empFields.put("email", "veronicammabatho@yahoo.com");
+        empFields.put("email", emailAddress);
         empFields.put("first_name",first_name);
-        empFields.put("last_name", "Mphahlele");
+        empFields.put("last_name", last_name);
         empFields.put("avatar", "https://reqres.in/img/faces/2-image.jpg");
 
         return empFields;
--- src/test/java/Common/TestDataCreation.java
+++ src/test/java/Common/TestDataCreation.java
@@ -8,5 +8,7 @@ public class TestDataCreation {
     static Faker faker = new Faker();
 
     public static String first_name = faker.name().firstName();
+    public static String last_name = faker.name().lastName();
+    public static String emailAddress = faker.internet().emailAddress();
 
 }
